b6567f2e3a51a16352e1931a64edbe6365a38212
Benjamin Rose <benjaminharringtonrose@gmail.com>
2021-03-22 21:26:27 -0400

    saving stuff

--- src/store/actions/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./AuthActions";
-export * from "./FeedActions";
-export * from "./FriendActions";
-export * from "./UserActions";
-export * from "./NotificationActions";
--- src/styles/index.tsx
+++
@@ -1,4 +0,0 @@
-export * from "./Color";
-export * from "./Spacing";
-export * from "./TextStyles";
-export * from "./ViewStyles";
